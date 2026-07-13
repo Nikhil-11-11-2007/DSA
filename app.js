47945df0407d505b9b0caf2ed3a4d05a692cff62
Nikhil gupta <ng364357@gmail.com>
2026-07-13 17:59:04 +0530

    refactor app.js to add commented-out functions for sum and greet examples

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-const temp = 10;
-console.log(temp)
+// let [a,b] = [10,20]
+// que1 sum of two integers
+// function sum(a, b) {
+//     // Write your logic here
+
+//     return console.log(a + b)
+
+// }
+
+// sum(10,20)
+
+// que2 Greet the user
+
+// function greet(name, age) {
+//     // Write your logic here
+//     console.log(`Hello ${name} you are ${age} years old.`)
+
+// }
+
+// greet("Shery",12)
