31dfdf5e346d886fdc347a35bdd34b1d939efc58
Nikhil gupta <ng364357@gmail.com>
2026-07-14 18:46:20 +0530

    add swapNumbers function with two methods for swapping values

--- app.js
+++ app.js
@@ -18,3 +18,21 @@
 // }
 
 // greet("Shery",12)
+
+// que 3
+
+function swapNumbers(a, b) {   
+    // 1st method
+    // let temp = a
+    // a = b
+    // b = temp
+
+    // 2nd method 
+
+    [a,b] = [b,a]
+
+    console.log(a,b)
+
+}
+
+swapNumbers(5,10)
