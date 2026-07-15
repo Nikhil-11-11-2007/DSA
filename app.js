1f9f70dced293bf18c6c31429dd45305f11e3586
Nikhil gupta <ng364357@gmail.com>
2026-07-15 12:43:45 +0530

    refactor app.js to comment out swapNumbers and other functions for clarity

--- app.js
+++ app.js
@@ -21,18 +21,54 @@
 
 // que 3
 
-function swapNumbers(a, b) {   
-    // 1st method
-    // let temp = a
-    // a = b
-    // b = temp
+// function swapNumbers(a, b) {   
+//     // 1st method
+//     // let temp = a
+//     // a = b
+//     // b = temp
 
-    // 2nd method 
+//     // 2nd method 
 
-    [a,b] = [b,a]
+//     [a,b] = [b,a]
 
-    console.log(a,b)
+//     console.log(a,b)
 
+// }
+
+// swapNumbers(5,10)
+
+// que 4
+
+// function calculateCompoundInterest(P, r, t, n) {
+//     // Write your logic here
+//     let A = P*Math.pow(1+r/n,(n*t))
+//     return (A - P).toFixed(2)
+// }
+
+// console.log(calculateCompoundInterest(1000,0.5,10,4))
+
+// que 5
+
+// function calculateTriangleArea(a, b, c) {
+//     // Write your logic here
+//     let s = (a+b+c)/2
+
+//     let A = Math.sqrt(s*(s-a)*(s-b)*(s-c))
+
+//     return A.toFixed(2)
+
+// }
+
+// console.log(calculateTriangleArea(3,4,5))
+
+// que 6
+
+function calculateCircleProperties(r) {
+    // Write your logic here
+    let circum = (2*Math.PI*r).toFixed(2)
+    let areaOfCircle = (Math.PI*r*r).toFixed(2)
+
+    return [circum, areaOfCircle]
 }
 
-swapNumbers(5,10)
+console.log(calculateCircleProperties(5.2))
