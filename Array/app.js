2e289f9a66b94a2789684f0579ad8f41b39c4ed2
Nikhil gupta <ng364357@gmail.com>
2026-08-08 12:47:04 +0530

    refactor app.js to add copyAndReverseArray and moveZerosToEnd functions with commented logic for future implementation

--- Array/app.js
+++ Array/app.js
@@ -76,3 +76,98 @@ let prompt = require("prompt-sync")()
 // }
 // console.log(findSecondGreatestElement(arr))
 
+// que 47
+
+// let arr = [1,2,3,4,5]
+
+function copyAndReverseArray(arr) {
+    // Write your logic here
+    // method-1
+    // let newArr = []
+
+    // for(let i = 0; i<arr.length; i++){
+    //     newArr.push(arr[arr.length - i - 1])
+    // }
+
+    // return newArr
+
+    // 2nd method 
+
+    // let temp = new Array(arr.length)
+
+    // let i = arr.length - 1
+
+    // for(let j = 0; j<arr.length; j++){
+    //     temp[j] = arr[i]
+    //     i--
+    // }
+    // return temp
+
+}
+
+// console.log(copyAndReverseArray(arr))
+
+// que 48
+
+// let arr = [1, 2, 3, 4, 5]
+
+// function reverseArray(arr) {
+
+//     // Write your code here
+
+//     let i = 0
+//     let j = arr.length - 1
+
+//     while (i < j) {
+//         [arr[i], arr[j]] = [arr[j], arr[i]]
+//         i++;
+//         j--
+//     }
+
+//     return arr
+
+// }
+
+// console.log(reverseArray(arr))
+
+// que 49
+
+let arr = [0, 1, 0, 1, 1]
+
+function moveZerosToEnd(arr) {
+    // Write Your Code Here
+    // let arr2 = []
+
+    // for(let i = 0; i<arr.length; i++){
+    //     if(arr[i] === 1){
+    //         arr2.push(1)
+    //     }
+    // }
+
+    // while(arr2.length < arr.length){
+    //     arr2.push(0)
+    // }
+
+    // return arr2
+
+    // let n = arr.length
+
+    // let arr2 = new Array(n)
+
+    // let i = 0, j = 0
+
+    // while(arr2.length < arr.length){
+
+    //     if(arr[i] === 0){
+    //         arr2[j] === arr[i]
+    //         i++;
+    //         j++;
+    //     }
+
+    // }
+
+    // return arr2
+
+}
+
+console.log(moveZerosToEnd(arr))
