4dc29e6216db55cfc88b46e3ff9d79d89a9ce342
Nikhil gupta <ng364357@gmail.com>
2026-08-12 16:50:58 +0530

    refactor app.js to improve sortHalves method in Solution class for better half sorting logic

--- Array/app.js
+++ Array/app.js
@@ -346,15 +346,22 @@ class Solution {
         // write your code here
         let n = Math.floor(arr.length / 2)
         for (let i = 0; i < n - 1; i++) {
-            if (arr[i + 1] < arr[i]) {
-                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
+            for (let j = i + 1; j < n; j++) {
+                if (arr[i] > arr[j]) {
+                    [arr[i],arr[j]] = [arr[j],arr[i]]
+                }
+
             }
         }
-        for (let i = n; i < arr.length-1; i++) {
-            if (arr[i] < arr[i + 1]) {
-                [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
+
+        for(let i = n; i<arr.length-1; i++){
+            for(let j = i+1; j<arr.length; j++){
+                if(arr[i]<arr[j]){
+                    [arr[i],arr[j]] = [arr[j],arr[i]]
+                }
             }
         }
+
 
         return arr
     }
@@ -362,31 +369,3 @@ class Solution {
 let halfSort = new Solution()
 console.log(halfSort.sortHalves([-4, -1, 3, 2, 10, 0]))
 
-
-// class Solution {
-//     sortHalf(arr) {
-//         // Write your code here
-//         let n = arr.length;
-//         let mid = Math.floor(n / 2);
-
-//         for(let i = mid; i < n - 1; i++){
-//             for(let j = i + 1; j < n; j++){
-//                 if(arr[i] < arr[j]){
-//                     [arr[i], arr[j]] = [arr[j], arr[i]];
-//                 }
-//             }
-//         }
-
-//         for(let i = 0; i < mid - 1; i++){
-//             for(let j = i + 1; j < mid; j++){
-//                 if(arr[i] > arr[j]){
-//                     [arr[i], arr[j]] = [arr[j], arr[i]];
-//                 }
-//             }
-//         }
-
-//         return arr;
-//     }
-// }
-
-// module.exports = { Solution };
