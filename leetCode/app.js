7348143a2970329fc809e2054137c66927c82d93
Nikhil gupta <ng364357@gmail.com>
2026-09-01 18:18:38 +0530

    refactor app.js to add lengthOfLongestSubstring function and comment out twoSum function

---
+++ leetCode/app.js
@@ -0,0 +1,37 @@
+let prompt = require("prompt-sync")()
+
+// que 1
+
+// var twoSum = function(nums, target) {
+
+//     for(let i = 0; i<nums.length; i++){
+//         for(let j = i+1; j<nums.length; j++){
+//             if(nums[i]+nums[j] === target){
+//                 return [i,j]
+//             }
+//         }
+//     }
+
+// };
+
+// console.log(twoSum([2,7,11,15],17))
+
+// que 2
+
+var lengthOfLongestSubstring = function (s) {
+    let logsubString = ""
+    let max = 0
+    for (let i = 0; i < s.length; i++) {
+        let ch = s[i]
+        if (!logsubString.includes(ch)) {
+            logsubString += ch
+        } else {
+            logsubString = logsubString.slice(logsubString.indexOf(ch) + 1) + ch
+        }
+        max = Math.max(max, logsubString.length)
+    }
+
+    return max
+};
+
+console.log(lengthOfLongestSubstring("abcabcbb"))
