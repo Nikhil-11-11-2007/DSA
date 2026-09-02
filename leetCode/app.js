b88a9a42f1f6a36885f92c33dcfa0d5d17f0e850
Nikhil gupta <ng364357@gmail.com>
2026-09-02 17:49:26 +0530

    refactor app.js to comment out lengthOfLongestSubstring function and update leftRotateByOne function

--- leetCode/app.js
+++ leetCode/app.js
@@ -18,20 +18,48 @@ let prompt = require("prompt-sync")()
 
 // que 2
 
-var lengthOfLongestSubstring = function (s) {
-    let logsubString = ""
-    let max = 0
-    for (let i = 0; i < s.length; i++) {
-        let ch = s[i]
-        if (!logsubString.includes(ch)) {
-            logsubString += ch
-        } else {
-            logsubString = logsubString.slice(logsubString.indexOf(ch) + 1) + ch
-        }
-        max = Math.max(max, logsubString.length)
+// var lengthOfLongestSubstring = function (s) {
+//     let logsubString = ""
+//     let max = 0
+//     for (let i = 0; i < s.length; i++) {
+//         let ch = s[i]
+//         if (!logsubString.includes(ch)) {
+//             logsubString += ch
+//         } else {
+//             logsubString = logsubString.slice(logsubString.indexOf(ch) + 1) + ch
+//         }
+//         max = Math.max(max, logsubString.length)
+//     }
+
+//     return max
+// };
+
+// console.log(lengthOfLongestSubstring("abcabcbb"))
+
+let arr = [1, 2, 3, 4, 5]
+
+function leftRotateByOne(arr) {
+    // Write your logic here
+    // let i = 1, j = 0
+
+    // while (i < arr.length) {
+    //     [arr[j], arr[i]] = [arr[i], arr[j]]
+    //     j++;
+    //     i++;
+    // }
+
+    // return arr
+
+    // 2nd method for interview
+
+    let temp = arr[0]
+
+    for(let i = 1; i<arr.length; i++){
+        arr[i-1] = arr[i]
     }
+    arr[arr.length - 1] = temp
 
-    return max
-};
+    return arr
+}
 
-console.log(lengthOfLongestSubstring("abcabcbb"))
+console.log(leftRotateByOne(arr))
