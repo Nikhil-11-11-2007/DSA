e2f7e01e658cabed616bdd56dd3fbc0048136475
Nikhil gupta <ng364357@gmail.com>
2026-09-07 14:03:12 +0530

    add 2D-Array.js with commented code for 2D array creation and display

---
+++ Array/2D-Array.js
@@ -0,0 +1,30 @@
+let prompt = require("prompt-sync")()
+
+// let arr = new Array(3)
+
+// for(let i = 0; i<arr.length; i++){
+//     arr[i] = new Array(3)
+// }
+
+// for(let i = 0; i<arr.length; i++){
+//     for(let j = 0; j<arr[i].length; j++){
+//         arr[i][j] = +prompt("Enter a number ")
+//     }
+// }
+
+// for(let i = 0; i<arr.length; i++){
+//     for(let j = 0; j<arr[i].length; j++){
+//         process.stdout.write(arr[i][j] + " ")
+//     }
+//     console.log()
+// }
+
+// let arr2 = Array.from({length:3}, () => new Array(3).fill(0))
+
+// for(let i = 0; i<arr2.length; i++){
+//     for(let j = 0; j<arr2[i].length; j++){
+//         process.stdout.write(arr2[i][j] + " ")
+//     }
+//     console.log()
+// }
+
