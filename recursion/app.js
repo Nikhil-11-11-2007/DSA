3ad088dc15ec84d5a182bdcbac4856c507dff169
Nikhil gupta <ng364357@gmail.com>
2026-09-25 13:13:59 +0530

    refactor app.js to comment out unused functions and implement fibonacci function for recursive calculation

--- recursion/app.js
+++ recursion/app.js
@@ -13,9 +13,90 @@ let prompt = require("prompt-sync")()
 
 // que 2
 
-function sumUptoN(n) {
-    if (n == 1) return n;
-    return n + sumUptoN(n - 1)
-}
+// function sumUptoN(n) {
+//     if (n == 1) return n;
+//     return n + sumUptoN(n - 1)
+// }
 
-console.log(sumUptoN(1))
+// console.log(sumUptoN(1))
+
+// que 3
+
+// function factorialOfN(n) {
+//     if (n == 1) return n;
+//     return n * factorialOfN(n - 1)
+// }
+
+// console.log(factorialOfN(5))
+
+// que 4
+
+/**
+ * @param {number} n
+ * @return {void}
+ */
+// var fibonacci = function (n) {
+//     // method 1 
+//     // let first = 0;
+//     // let second = 1;
+
+//     // process.stdout.write(first + " " + second + " ");
+
+//     // for (let i = 0; i < n - 2; i++) {
+//     //     let third = first + second;
+
+//     //     process.stdout.write(third + " ");
+
+//     //     first = second;
+//     //     second = third;
+//     // }
+
+//     // ye number mai output dega loop se hi
+
+//     // if (n <= 1) return n;
+
+//     // let first = 0;
+//     // let second = 1;
+
+//     // for (let i = 0; i < n - 1; i++) {
+//     //     let third = first + second;
+
+//     //     first = second;
+//     //     second = third;
+//     // }
+
+//     // return second;
+
+//     // method 2 recursion real use
+
+//     let first = 0, second = 1
+
+//     process.stdout.write(first + " " + second + " ")
+
+//     function generate(n, first, second) {
+//         if (n <= 0) return;
+//         let third = first + second
+//         process.stdout.write(third + " ")
+//         first = second
+//         second = third
+
+//         generate(n - 1, first, second)
+//     }
+
+//     generate(n - 2, first, second)
+
+
+// };
+
+// fibonacci(5)
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n) {
+    if (n <= 1) return n;
+    return fib(n - 1) + fib(n - 2);
+};
+
+console.log(fib(3))
